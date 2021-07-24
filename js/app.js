9c1800a77480a75111ec75170b32bd094c099416
shubhamkumar1920 <kshubham291@gmail.com>
2021-07-25 01:24:21 +0530

    in

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        prevArrow:'.prev-btn',
+        nextArrow:'.next-btn',
+        dots:true,
+        appendDots:'.slider-dots',
+        autoplay:true,
+        fade:true,
+        autoplaySpeed:'6000',
+        
+    });
+});
